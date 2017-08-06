7fd4dd8c779ae1dd1150687e44a7054d34512984
Luiz Cláudio <luizclaudioss.97@gmail.com>
2017-08-06 12:19:25 -0300

    Ajustes

--- js/dash_js.js
+++ js/dash_js.js
@@ -14,3 +14,20 @@ $(".menu-container ul li a").on('click', function(){
 	$("#"+tab_id).addClass('ativa');
 });
 
+
+$(".nav-tabs li a").on('click', function(){
+	$(this)
+	.parent().parent()
+	.find(".active").removeClass('active');
+	$(this).parent().addClass('active');
+
+	var tab_id = $(this).attr('data-tab');
+	
+	$('.nav-tabs ul li').removeClass('aberta');
+	$('.tab-result').removeClass('aberta');
+
+	
+	$("#"+tab_id).addClass('aberta');
+
+
+});
