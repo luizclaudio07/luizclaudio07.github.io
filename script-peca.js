0e1a46de90b71b0c4a37c156345c4165243b711f
Luiz Claudio <luiz.santos@pdcase.com.br>
2023-08-24 21:38:36 -0300

    sincronizando

--- script-peca.js
+++ script-peca.js
@@ -35,25 +35,66 @@ images.forEach((image, index) => {
 });
 
 
-function getCategoriaParam() {
+function getParam(param) {
   const queryString = window.location.search;
   const params = new URLSearchParams(queryString);
-  return params.get('categoria');
+  return params.get(param);
 }
 
 
-
-
-function montarPeça(){
-  
-  fetch('https://raw.githubusercontent.com/luizclaudio07/luizclaudio07.github.io/master/bd.json?v='+Math.floor(Math.random() * 999)) 
+function carregaJSON(){
+  fetch('https://raw.githubusercontent.com/luizclaudio07/luizclaudio07.github.io/master/bd-pecas.json?v='+Math.floor(Math.random() * 999)) 
   .then(response => response.json())  
   .then(data =>  { 
-    var bicho = data.find(x => x.categoria == getCategoriaParam()) ;
+
+    listaPecas = data.find(x => x.categoria == getParam('categoria'));
+
+    if(!listaPecas){
+      window.location.href = 'index.html'
+    }
+  
+    pecaJSON = listaPecas.items.find(x => x.id == getParam('id'));
+
+    if(!pecaJSON){
+      window.location.href = 'index.html'
+    }
+
+
+    var listFotos = [];
+
+    for(var i = pecaJSON.qtdFotos; i != 0; i--){
+      listFotos.push('FOTOS/'+getParam('categoria')+'/'+getParam('id')+'/acervo ('+i+').jpg')
+    }
+
+    listFotos = listFotos.reverse()
+    
+    var templateFotosCarroussel = `<div class="carousel-item [FLAGACTIVE]"><img class="d-block w-100 gallery-item" src="[URLFOTO]" alt="Foto"></div>`
+    var templateFotosBook = `<div class="col-md-4 col-sm-12"><img class="d-block w-100 gallery-item" src="[URLFOTO]" alt="Second slide"></div>`;
+
+    var conteudoCarrossel = document.getElementById('conteudoCarrossel');
+    var conteudoBook = document.getElementById('conteudoBook');
+
+
+    listFotos.forEach((item, index) => {
+      conteudoCarrossel.innerHTML += templateFotosCarroussel.replace('[URLFOTO]', item).replace('[FLAGACTIVE]', index == 0 ? 'active': '')
+      conteudoBook.innerHTML += templateFotosBook.replace('[URLFOTO]', item)
+    })
+
+
+    var areaInfo = document.getElementById('areaInfo');
+
+    areaInfo = areaInfo.replace('[NOMEPECA]', pecaJSON.nomePeca).replace('[DESCRICAO]', pecaJSON.descricao)
+
+    
+
+
+
   })
   .catch(error => { console.error('Erro ao carregar o arquivo JSON:', error) });
-  
+}
 
+window.onload = function(){
+  carregaJSON()
 }
 
 
