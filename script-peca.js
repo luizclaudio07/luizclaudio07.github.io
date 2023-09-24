bd2aa76e1ae587d775065d3d4b3fbba56d35ebeb
Luiz Claudio <luiz.santos@pdcase.com.br>
2023-09-23 23:50:40 -0300

    correcoes

--- script-peca.js
+++ script-peca.js
@@ -10,6 +10,25 @@ function getParam(param) {
   return params.get(param);
 }
 
+function voltarPagina(){
+  history.back()
+}
+
+function mostrarTexto() {
+  var divTexto = document.getElementById("texto");
+  var botaoVerMais = document.getElementById("ver-mais");
+
+  if (divTexto.style.maxHeight === "100px") {
+    divTexto.style.maxHeight = "none";
+    divTexto.style.marginBottom  = "25px"
+    botaoVerMais.innerHTML = "<i class=\"fa-solid fa-minus\"></i> Ver menos";
+  } else {
+    divTexto.style.marginBottom  = "0px"
+    divTexto.style.maxHeight = "100px";
+    botaoVerMais.innerHTML = "<i class=\"fa-regular fa-plus\"></i> Ver mais";
+  }
+}
+
 
 function carregaJSON(){
   fetch('https://raw.githubusercontent.com/luizclaudio07/luizclaudio07.github.io/master/bd-pecas.json?v='+Math.floor(Math.random() * 999)) 
@@ -64,7 +83,7 @@ function carregaJSON(){
     var areaInfo = document.getElementById('taxo');
 
 
-    const templateTaxo = "<span class=\"badge  badge-secondary\"><b>[NOMETAXO]:</b> [VALORTAXO]</span>";
+    const templateTaxo = "<span class=\"badge col-12 mb-2 badge-pill badge-secondary\"><i class=\"fa-solid fa-hand-point-right\"></i> <span class=\"font-weight-bold\">[NOMETAXO]:</span> <span class=\"font-weight-light\">[VALORTAXO]</span></span>";
 
 
     if(pecaJSON.reino){
